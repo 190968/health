d6d20b9d0f1520a68e7bcb8fcbcb8afe09b26906
ED <e@fitango.com>
2018-02-26 21:52:35 +0300

    tracker input fix

--- src/routes/Plan/components/Tracker/index.js
+++ src/routes/Plan/components/Tracker/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import styles from './index.less';
 import VMasker from "vanilla-masker";
-import { InputNumber } from 'antd';
+import { Input } from 'antd';
 
 export default class Tracker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            //   tab:''
+            value:props.value
         };
         this.onChange = this.onChange.bind(this);
     };
@@ -21,32 +21,34 @@ export default class Tracker extends React.Component {
         value: ''
     };
 
-    onChange(value) {
+    onChange(event) {
+        const {inputMask} = this.props.item;
+
+        let value = event.target.value;
+        value = inputMask != '' ? VMasker.toPattern(value, inputMask) : VMasker.toNumber(value);
+        this.setState({value:value});
         if (typeof this.props.onChange === 'function') {
             this.props.onChange(value);
         }
     }
 
     render() {
-        const {item, value} = this.props;
+        const {item} = this.props;
+        const {value} = this.state;
 
         const {units, inputMask} = item;
         const unitsName = units.name;
+        let className = "tracker-input"+(value ? ' tracker-input-reported' : '');
 
-        return (<InputNumber
+        return (<Input className={className}
             value={value}
             //formatter={value => value.replace(/\B(?=(\d{3})+(?!\d))/g, '/')}
             formatter={value => {
-
-
                 if (inputMask != '') {
-
                     return VMasker.toPattern(value, inputMask)
                 } else {
-
                     return VMasker.toNumber(value);
                 }
-
             }}
             //parser={value => VMasker.toPattern(value, inputMask)}
             //parser={value => value.replace(/\$\s?|(,*)/g, '')}
@@ -82,8 +84,10 @@ export class TrackerUncontrolled extends React.PureComponent {
         const {value} = this.state;
         const {units, inputMask} = item;
         const unitsName = units.name;
+        console.log(value);
+        let className = "tracker-input"+(value ? ' tracker-input-reported' : '');
 
-        return (<InputNumber
+        return (<Input className={className}
             value={value}
             //formatter={value => value.replace(/\B(?=(\d{3})+(?!\d))/g, '/')}
             formatter={value => {
