4fc06bb55cd799df37ab0e7abac338a56b1a3569
ED <e@fitango.com>
2018-03-01 20:05:37 +0300

    avatar change

--- src/routes/User/components/Settings/components/Picture/components/uploadImage.js
+++ src/routes/User/components/Settings/components/Picture/components/uploadImage.js
@@ -8,83 +8,85 @@ const Transloadit = require('uppy/lib/plugins/Transloadit')
 const Webcam = require('uppy/lib/plugins/Webcam')
 const Url = require('uppy/lib/plugins/Url')
 const GoogleDrive = require('uppy/lib/plugins/GoogleDrive')
-
-const uppy = Uppy({
-    meta: { type: 'avatar' },
-    restrictions: { maxNumberOfFiles: 1 },
-    autoProceed: true
-})
-
-//uppy.use(Tus, { endpoint: '/upload' })
-uppy.use(Transloadit, {
-    // Transloadit plugin options
-    params: {
-        auth: { key: 'f0ae93b755c346b4864e79e9ac3613ed' },
-        template_id:  '80ee9b46f43f47969dea1599e77c7721'
-    },
-    waitForEncoding: true
-}).on('transloadit:result', (assembly, fileIDs) => {
-    console.log(assembly);
-    console.log(fileIDs);
-
-
-    if ( dashboard.isModalOpen() ) {
-        dashboard.closeModal()
-    }
-    /*console.group('Created', assembly.assembly_id, 'for files:')
-    console.log(assembly);
-    console.log(fileIDs);
-    for (const id of fileIDs) {
-        console.log(uppy.getFile(id).name)
-    }
-    console.groupEnd()*/
-}).use(Webcam)
-/*
-uppy.on('complete', (result) => {
-    console.log(result);
-    /*const url = result.successful[0].uploadURL
-    store.dispatch({
-        type: SET_USER_AVATAR_URL,
-        payload: { url: url }
-    })*/
-//})*/
-
-uppy.run();
-
-const dashboard = uppy.getPlugin('Dashboard')
+const Dropbox = require('uppy/lib/plugins/Dropbox')
 
  class AvatarPicker extends React.Component {
-     state = {
-         modalOpen: false
+
+     static defaultProps = {
+         template:'',
+         waitForEncoding:true,
+         maxNumberOfFiles:1,
+         simpleResult:false// if result should be just Key-value
      }
 
-     handleOpen =  () => {
-         this.setState({
-             modalOpen: true
+     getTemplateId = () => {
+         switch(this.props.template) {
+             case 'userpic':
+                 return 'c3fa91f909b04bbd9e49816ef20bae5c';
+                 break;
+         }
+
+     }
+     componentWillMount () {
+        const {onComplete, simpleResult, maxNumberOfFiles} = this.props;
+         this.uppy = new Uppy({
+             //meta: { type: 'avatar' },
+             restrictions: { maxNumberOfFiles: this.props.maxNumberOfFiles },
+             autoProceed: true
+         }).use(Transloadit, {
+             // Transloadit plugin options
+             params: {
+                 auth: { key: 'f0ae93b755c346b4864e79e9ac3613ed' },
+                 template_id:  this.getTemplateId()
+             },
+             waitForEncoding: this.props.waitForEncoding// we need this only for images and files - we don't need that for video!
+             //waitForMetadata:true
          })
+         /* .on('transloadit:result', (stepName, result) => {
+              //const file = uppy.getFile(result.localId)
+              console.log(stepName);
+              console.log(result);
+          })*/
+          .on('transloadit:complete', (assembly) => {
+          //console.log(assembly);
+            let {results} = assembly;
+            //console.log(results);
+            if (simpleResult) {
+                if (maxNumberOfFiles === 1) {
+
+                    const newResults = {};
+                    results = Object.keys(results).map(key => {
+                        const infos = results[key];
+                        const info = infos[0];
+
+                        newResults[key] = info.signed_ssl_url;
+                        return null;
+                    })
+                    results = newResults;
+                }
+            }
+              //console.log(results);
+              onComplete(results);
+
+      }).use(Webcam).use(GoogleDrive).use(Dropbox)
+             .run()
      }
 
-     handleClose = () => {
-         this.setState({
-             modalOpen: false
-         })
+     componentWillUnmount () {
+         this.uppy.close()
      }
 
      render () {
          return (
-             <div>
-
-                 <Button onClick={this.handleOpen}>Change avatar</Button>
                  <DashboardModal
                      disableThumbnailGenerator
                      hideProgressAfterFinish
-                     uppy={uppy}
+                     uppy={this.uppy}
                      closeModalOnClickOutside
-                     open={this.state.modalOpen}
+                     open={true}
                      onRequestClose={this.handleClose}
-                     plugins={['Webcam']}
+                     plugins={['Webcam', 'GoogleDrive', 'Dropbox']}
                  />
-             </div>
          )
      }
 }
