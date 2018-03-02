5f1159ff9f82ea2b0179ba6156ff9fcdb165fedd
ED <e@fitango.com>
2018-03-02 13:07:12 +0300

    avatar fix

--- src/routes/User/components/Avatar/index.js
+++ src/routes/User/components/Avatar/index.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import {Avatar as AvatarAntd} from 'antd';
+import {Link} from 'react-router-dom';
 import './index.less';
 
 class Avatar extends React.PureComponent {
@@ -10,11 +11,12 @@ class Avatar extends React.PureComponent {
     static defaultProps = {
         size: 'small',
         src: '',
+        useLink: false,
         info: {},
     }
     render() {
 
-        const {src,info, size} = this.props;
+        const {src, info, size, useLink} = this.props;
 
         // if we need a realy big avatar(profile page)
         const extraClass = size === 'huge' ? 'ant-avatar-huge' : '';
@@ -26,14 +28,17 @@ class Avatar extends React.PureComponent {
         const {thumbs={}} = info;
         const {small=''} = thumbs;
         const name = info.firstName ? info.firstName : ((info.fullName && info.fullName !== ' ') ? info.fullName : '');
-        console.log(name);
-        return (
+        //console.log(name);
+        let avatar =
                 <AvatarAntd src={small} className={extraClass} size={size} style={{verticalAlign: 'middle', backgroundColor: info.color}}>
                     {name ? name[0] :
                         (info.email ? info.email[0] : 'N/A' )}
-                </AvatarAntd>
+                </AvatarAntd>;
 
-        );
+        if (useLink) {
+            avatar = <Link to={'/u/'+info.id}>{avatar}</Link>;
+        }
+        return (avatar);
     }
 }
 
